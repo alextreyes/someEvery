5c5ed403a499d0e1f2d6561ecdc770ea21e1b235
Alex Reyes <alextreyes11@gmail.com>
2023-08-23 00:49:08 -0400

    finished

--- app.js
+++ app.js
@@ -15,3 +15,45 @@ function myEvery(arr, func) {
   }
   return true;
 }
+
+function hasOddNumber(arr) {
+  return arr.some(function (number) {
+    if (number % 2 !== 0) {
+      return true;
+    }
+  });
+}
+
+function hasZero(numbers) {
+  const numStr = numbers.toString();
+  return numStr.includes("0");
+}
+
+function hasOnlyOddNumbers(arr) {
+  return arr.every(function (number) {
+    return number % 2 !== 0;
+  });
+}
+
+function hasNoDups(arr) {
+  let a = [];
+  return arr.every(function (number) {
+    if (a.indexOf(number) !== -1) {
+      return false;
+    }
+    a.push(number);
+    return true;
+  });
+}
+
+function hasCertainKey(arr, key) {
+  return arr.every(function (obj) {
+    return obj.hasOwnProperty(key);
+  });
+}
+
+function hasCertainValue(arr, key, value) {
+  return arr.every(function (obj) {
+    return obj.hasOwnProperty(key) && obj[key] === value;
+  });
+}
